5347d2fe0d96856de6fa631c9d375dd987d22b56
190n <benjamin.j.grant@gmail.com>
2019-04-20 15:59:26 -0700

    Initial version

--- README.md
+++ README.md
@@ -39,4 +39,44 @@ const scrollclass = require('scrollclass');
 Usage
 -----
 
-coming soon
+See [test.html](https://github.com/190n/scrollclass/blob/master/test.html) for a
+complete example.
+
+```js
+
+// scrollclass accepts a list of triggers
+let sc = scrollclass([
+    {
+        // element to apply the class to
+        target: document.getElementById('target'),
+        // which class to add or remove
+        class: 'active',
+        // it will have the class when at least 75% of the trigger element is visible
+        // default: 0
+        threshold: 0.75,
+        // which element to check visibility of
+        // if not specified, the target element is used
+        trigger: document.getElementById('trigger'),
+        // once = true: once the target element is first given the class, it will never be removed
+        // once = false (default): the class will be removed when the condition is no longer met
+        once: false,
+        // class the target element should have when the trigger is not visible
+        // by default, no class will be added (the main class will just be removed)
+        alternateClass: 'inactive',
+        // inverts all operations
+        // default: false
+        invert: false,
+        // margin to add around the trigger element's bounding box
+        // specified the same way as the CSS margin property (top right bottom left)
+        // default: '0px'
+        margin: '20px',
+        // root element that visibility is computed relative to
+        // must be an ancestor of the trigger element
+        // default: browser viewport
+        root: document.getElementById('rootElem')
+    } // can specify as many more as you want, or make multiple calls to scrollclass
+]);
+
+// disable all triggers without changing classes on any target elements
+sc.disable();
+```
--- scrollclass.js
+++ scrollclass.js
@@ -1,7 +1,70 @@
 (function() {
 
-    function scrollclass() {
+    function scrollclass(triggers) {
+        let created = [];
 
+        for (let t of triggers) {
+            let target = t.target,
+                className = t.class,
+                threshold = t.hasOwnProperty('threshold') ? t.threshold : 0,
+                trigger = t.hasOwnProperty('trigger') ? t.trigger : target,
+                once = t.hasOwnProperty('once') ? t.once : false,
+                alternateClass = t.hasOwnProperty('alternateClass') ? t.alternateClass : null,
+                invert = t.hasOwnProperty('invert') ? t.invert : false,
+                margin = t.hasOwnProperty('margin') ? t.margin : '0px',
+                root = t.hasOwnProperty('root') ? t.root : null;
+
+            let options = {
+                root,
+                threshold,
+                rootMargin: margin
+            };
+
+            let observer = new IntersectionObserver((entries, observer) => {
+                let entry = entries[0],
+                    shouldHaveClass = entry.isIntersecting && entry.intersectionRatio >= threshold;
+
+                if (invert) {
+                    shouldHaveClass = !shouldHaveClass;
+                }
+
+                if (shouldHaveClass) {
+                    target.classList.add(className);
+                    target.classList.remove(alternateClass);
+
+                    if (once) {
+                        observer.disconnect();
+                    }
+                } else {
+                    target.classList.remove(className);
+                    if (alternateClass !== null) {
+                        t.target.classList.add(alternateClass);
+                    }
+                }
+            }, options);
+
+            observer.observe(trigger);
+            created.push({
+                observer,
+                target,
+                className,
+                threshold,
+                trigger,
+                once,
+                alternateClass,
+                invert,
+                margin,
+                root
+            });
+        }
+
+        return {
+            disable() {
+                for (let t of created) {
+                    t.observer.disconnect();
+                }
+            }
+        }
     }
 
     if (typeof this.module == 'object') {
